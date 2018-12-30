d386446046dbbf48f04d3e896b13bcffb53a22fa
AAurelioGN <36158509+alejandro61299@users.noreply.github.com>
2018-12-30 14:29:28 +0100

    Updated light position

--- Tails.io/ModulePlayer.cpp
+++ Tails.io/ModulePlayer.cpp
@@ -227,16 +227,8 @@ update_status ModulePlayer::Update(float dt)
 	playerCar->Brake(brake);
 
 	// Update light -----------------------------------------------
-	btVector3 offset(0,0,1.5f);
-	btVector3 Z(0, 0, 1);
-	btQuaternion q = playerCar->vehicle->getChassisWorldTransform().getRotation();
-
-	offset = offset.rotate(q.getAxis(), q.getAngle());
-	Z = Z.rotate(q.getAxis(), q.getAngle());
-	vec3 lightPos = playerCar->GetPos() + vec3(offset.getX(), offset.getY(), offset.getZ());
-
-	App->renderer3D->lights[playerNum - 1].SetPos(lightPos.x , lightPos.y, lightPos.z);
-	App->renderer3D->lights[playerNum].SetPos(lightPos.x, lightPos.y, lightPos.z);
+	vec3 lightPos = playerCar->GetPos() + vec3(0, 3, 0);
+	App->renderer3D->lights[playerNum - 1].SetPos(lightPos.x, lightPos.y, lightPos.z);
 	// Update Missiles  -------------------------------------------
 
 	p2List_item<Missile*> * item = missiles.getFirst() ;
